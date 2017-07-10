a6e68bb754713113303ffd970c9ffef1c500375f
J-Ibarra <ibarrajj@gmail.com>
2017-07-10 13:33:28 -0400

    Canvas and JFrame

--- src/org/jcs/MainKt.kt
+++ src/org/jcs/MainKt.kt
@@ -1,8 +1,17 @@
 package org.jcs
 
 import org.jcs.engine.GameLoop
+import java.awt.BorderLayout
+import java.awt.Canvas
+import java.awt.Dimension
+import javax.swing.JFrame
 
-class MainKt : IGameLogic {
+
+class MainKt : Canvas(), IGameLogic {
+
+    val SCALE = 3
+    val WIDTH = 640 / SCALE
+    val HEIGHT = 480 / SCALE
 
     override fun init() {
         println("init")
@@ -30,6 +39,20 @@ fun main(args: Array<String>) {
     val game = MainKt()
 
     val gameLoop = GameLoop(game)
+
+    val dimension: Dimension = Dimension(game.WIDTH * game.SCALE, game.HEIGHT * game.SCALE)
+    game.preferredSize = dimension
+    game.minimumSize = dimension
+    game.maximumSize = dimension
+
+    val frame: JFrame = JFrame("Kotlin Game")
+    frame.layout = BorderLayout()
+    frame.add(game, BorderLayout.CENTER)
+    frame.pack()
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.setLocationRelativeTo(null)
+    frame.isResizable = false
+    frame.isVisible = true
 
     gameLoop.start()
     Thread.sleep(10000)
